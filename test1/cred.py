48e15b3be010f3ebc5e5939e23f7d624f841424b
JashuTamil <jashu.tamil@gmail.com>
2023-02-22 15:42:35 -0500

    The start of something big
    I tried to turn the limit of current_user_recently_played to 50, but it doesn't seem to work. Maybe its because I never use Spotify. I have to look into this.

---
+++ test1/cred.py
@@ -0,0 +1,3 @@
+client_ID = "e3adb77535dd4e0186f29e81979b42d4"
+client_SECRET = "30aba4f606294a33ad00f6f06d9b32b5"
+redirect_url = "https://accounts.spotify.com/authorize?client_id=e3adb77535dd4e0186f29e81979b42d4&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%2F&scope=user-read-recently-played"
