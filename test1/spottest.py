a1ef5bf96b24990b6f9efbd28d9581b8d9f49a7c
JashuTamil <jashu.tamil@gmail.com>
2023-02-22 23:38:50 -0500

    I fixed it
    I managed to make a version of the app where it adds the top tracks of whatever the person listens to and just outputs it to the user. Yay.

--- test1/spottest.py
+++ test1/spottest.py
@@ -5,11 +5,21 @@ import cred
 scope = "user-top-read"
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
 
-results = sp.current_user_top_tracks()
+results = sp.current_user_top_tracks(limit = 20, time_range = "long_term")
 
-print(results)
 
 for idx, item in enumerate(results["items"]):
-    tracks = item["track"]
+    artists = ""
+    tracks = item["id"]
     trackInfo = sp.track(tracks)
-    print(trackInfo)
+
+    for x in range(len(trackInfo["artists"])):
+        if x != len(trackInfo["artists"]) - 1:
+            artists += trackInfo["artists"][x]["name"] + ", "
+        else:
+            if len(artists) == 0:
+                artists += trackInfo["artists"][x]["name"]
+            else:
+                artists += "and " + trackInfo["artists"][x]["name"]
+                
+    print(idx + 1, trackInfo["name"], "from", trackInfo["album"]["name"], "by", artists)
