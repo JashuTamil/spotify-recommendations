b802c62edc81d3774f67254c2d7f0eac7ec605f8
JashuTamil <jashu.tamil@gmail.com>
2023-02-22 22:10:53 -0500

    Problem
    Key value error. Got to look into that.

--- test1/spottest.py
+++ test1/spottest.py
@@ -2,12 +2,14 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import cred 
 
-scope = "user-read-recently-played"
+scope = "user-top-read"
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
 
-results = sp.current_user_recently_played()
+results = sp.current_user_top_tracks()
 
+print(results)
 
 for idx, item in enumerate(results["items"]):
-    track = item["track"]
-    print(idx + 1, track["artists"][0]["name"], " - ", track["name"])
+    tracks = item["track"]
+    trackInfo = sp.track(tracks)
+    print(trackInfo)
