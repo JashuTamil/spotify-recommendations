828d531dcd564778d6f9ea7d3982a4085d453e59
JashuTamil <jashu.tamil@gmail.com>
2023-02-23 22:41:10 -0500

    tried something new and failed.

--- brynaAcc/credBryna.py
+++
@@ -1,3 +0,0 @@
-client_ID = "3549e5d78d5a47acbfce6da7df8846d2"
-client_SECRET = "300aea65c62341d89c3d54b29dd0aef0"
-redirect_url = "https://localhost:"
--- test1/spottest.py
+++ test1/spottest.py
@@ -2,16 +2,27 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import cred
 
-scope = "user-read-recently-played"
+scope = "user-top-read"
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
 
-results = sp.current_user_recently_played(limit = 20)
+results = sp.current_user_top_tracks(limit = 20, time_range = "long_term")
 
 
-results = sp.current_user_recently_played()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " - ", track['name'])
+for idx, item in enumerate(results["items"]):
+    artists = ""
+    tracks = item["id"]
+    trackInfo = sp.track(tracks)
+
+    for x in range(len(trackInfo["artists"])):
+        if x != len(trackInfo["artists"]) - 1:
+            artists += trackInfo["artists"][x]["name"] + ", "
+        else:
+            if len(artists) == 0:
+                artists += trackInfo["artists"][x]["name"]
+            else:
+                artists += "and " + trackInfo["artists"][x]["name"]
+                
+    print(idx + 1, trackInfo["name"], "from", trackInfo["album"]["name"], "by", artists)
 
 
     """
