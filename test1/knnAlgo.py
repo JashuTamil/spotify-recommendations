b51e18a1d788fa3436f3fe33e4becfeb68d25a2c
JashuTamil <jashu.tamil@gmail.com>
2023-03-27 15:37:59 -0400

    Error on KNN
    For some reason, the KNN algorithm refuses to take in continuous data. I don't know why, but I have to look into changing that.

--- test1/knnAlgo.py
+++ test1/knnAlgo.py
@@ -10,10 +10,14 @@ danceability, energy, key, loudness, speechiness, acousticness, instrumentalness
 x = list(zip(danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, time_signature))
 y = ranking
 
+print(x)
+
+
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = .1)
 
 model = KNeighborsClassifier(n_neighbors = 2)
 
 model.fit(x_train, y_train)
-acc = model.score(x_test, y_test)
+y_pred = model.predict(x_test)
+acc = model.accuracy_score(y_test, y_pred)
 print(acc)
