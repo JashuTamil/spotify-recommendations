ae0d70754293a98c7728e83f3098e60dabe23950
JashuTamil <jashu.tamil@gmail.com>
2023-03-26 22:10:13 -0400

    Normalized the data

--- test1/knnAlgo.py
+++ test1/knnAlgo.py
@@ -4,7 +4,7 @@ import sklearn
 from sklearn.neighbors import KNeighborsClassifier
 
 arrtibutes = ["danceability", "energy", "key", "loudness", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "duration", "time_signature", "ranking"]
-audioPickle = open("audio.pkl", "rb")
+audioPickle = open("normalAudio.pkl", "rb")
 danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, time_signature, ranking = pickle.load(audioPickle)
 
 x = list(zip(danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, time_signature))
@@ -12,7 +12,7 @@ y = ranking
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = .1)
 
-model = KNeighborsClassifier(n_neighbors = 12)
+model = KNeighborsClassifier(n_neighbors = 2)
 
 model.fit(x_train, y_train)
 acc = model.score(x_test, y_test)
--- test1/normalization.py
+++ test1/normalization.py
@@ -1,6 +1,35 @@
 import pickle
 
+def normalizeAtttibute(attribute):
+    minimum = min(attribute)
+    range = max(attribute) - min(attribute)
+    normalized = []
+    for i in attribute:
+        num = i - minimum
+        num /= range
+        normalized.append(num)
+    return normalized
+
+
 audioPickle = open("audio.pkl", "rb")
 danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, time_signature, ranking = pickle.load(audioPickle)
 
 print(danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, time_signature, ranking)
+
+danceability = normalizeAtttibute(danceability)
+energy = normalizeAtttibute(energy)
+key = normalizeAtttibute(key)
+loudness = normalizeAtttibute(loudness)
+speechiness = normalizeAtttibute(speechiness)
+acousticness = normalizeAtttibute(acousticness)
+instrumentalness = normalizeAtttibute(instrumentalness)
+liveness = normalizeAtttibute(liveness)
+valence = normalizeAtttibute(valence)
+tempo = normalizeAtttibute(tempo)
+duration = normalizeAtttibute(duration)
+time_signature = normalizeAtttibute(time_signature)
+ranking = normalizeAtttibute(ranking)
+
+print(danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, time_signature, ranking)
+
+pickle.dump((danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, time_signature, ranking), open("normalAudio.pkl", "wb"))
