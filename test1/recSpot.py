e99643158d6980377a32b084fdec1ae12956eaf7
JashuTamil <jashu.tamil@gmail.com>
2023-04-13 21:19:54 -0400

    I figured it out again
    I REALLY hate spotify

--- test1/recSpot.py
+++ test1/recSpot.py
@@ -10,6 +10,6 @@ print(song_id)
 
 scope = "playlist-modify-private"
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
-recom = sp.recommendations(limit = 1, seed_artists = ["6rniTPs9zN26kYnkPdFl1U"], seed_genres = ["rock"], seed_tracks = ['0VjIjW4GlUZAMYd2vXMi3b', '0MNNKSUU9OOQ8DSGWduw79', '3xKsf9qdS1CyvXSMEid6g8'])
+recom = sp.recommendations(limit = 1, seed_tracks = song_id[0:5])
 
 print(recom)
