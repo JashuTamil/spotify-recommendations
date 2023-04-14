456901c91a217ee4e4ffd576a02181976e7093b3
JashuTamil <jashu.tamil@gmail.com>
2023-04-13 21:01:58 -0400

    Figured it out
    I hate spotify

--- test1/recSpot.py
+++ test1/recSpot.py
@@ -5,9 +5,11 @@ import cred
 
 file = open("song_ids", "rb")
 song_id = pickle.load(file)
+print(song_id)
+
 
 scope = "playlist-modify-private"
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
-recom = sp.recommendations(limit = 5, seed_artists = ["6rniTPs9zN26kYnkPdFl1U"], seed_genres = ["r&b", "pop", "hip-hop"], seed_tracks = [song_id[0]])
+recom = sp.recommendations(limit = 1, seed_artists = ["6rniTPs9zN26kYnkPdFl1U"], seed_genres = ["rock"], seed_tracks = ['0VjIjW4GlUZAMYd2vXMi3b', '0MNNKSUU9OOQ8DSGWduw79', '3xKsf9qdS1CyvXSMEid6g8'])
 
 print(recom)
