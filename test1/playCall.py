6b97b8d4603a89c43fd9ca4221ab2b3d5e638551
JashuTamil <jashu.tamil@gmail.com>
2023-05-18 11:43:31 -0400

    Fixed some quirks
    I'm trying to get more accurate recommendations for the music I want. I'm trying to now get the IDs from the music I want and display the song name.

--- test1/playCall.py
+++ test1/playCall.py
@@ -9,7 +9,7 @@ def playlistCall():
     sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
 
     playlists = sp.current_user_playlists()
-
+    print(playlists)
     return (playlists['items'][0]['id'])
 
 if __name__ == "__main__":
--- test1/playSongs.py
+++ test1/playSongs.py
@@ -8,13 +8,14 @@ from playCall import playlistCall
 def playlistSongs():
     song_id = []
     id = playlistCall()
+    
 
     scope = "playlist-modify-private"
     sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
 
     items = sp.playlist_items(id)
+    print(items)
     songs = items['items']
-    print(songs)
     
     for i in range(len(songs)):
         id = songs[i]['track']['id']
