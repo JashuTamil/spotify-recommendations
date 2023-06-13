eba0bbed8e5863714022f7b7991f73c3726977c6
JashuTamil <jashu.tamil@gmail.com>
2023-06-13 09:18:15 -0400

    Update popup.py

--- test1/popup.py
+++ test1/popup.py
@@ -1,6 +1,14 @@
 import pickle
 import tkinter
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import cred
 
 file = open("song_ids", "rb")
 ids = pickle.load(file)
 print(ids)
+
+scope = "playlist-modify-private"
+sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
+
+
