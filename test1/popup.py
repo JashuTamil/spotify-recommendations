bea23db23092801c4f52b5b0f228ccecf1074d9a
JashuTamil <jashu.tamil@gmail.com>
2023-06-22 09:46:02 -0400

    Added new elements
    Started to add the tracks and get details.

--- test1/popup.py
+++ test1/popup.py
@@ -3,12 +3,20 @@ import tkinter
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import cred
+import recSpot
 
 file = open("song_ids", "rb")
 ids = pickle.load(file)
-print(ids)
+
 
 scope = "playlist-modify-private"
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = cred.client_ID, client_secret = cred.client_SECRET, redirect_uri = cred.redirect_url, scope = scope))
 
+if len(ids) == 0:
+    recSpot.spotRecom()
+    file = open("song_ids", "rb")
+    ids = pickle.load(file)
 
+details = sp.track(ids.pop(0))
+print(details)
+print(ids)
